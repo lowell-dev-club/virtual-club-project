21658945daf9fc5062417358de30fab66c013504
Savagecoder77 <savagecoder77@gmail.com>
2020-05-22 16:13:23 -0700

    new code

--- project/views.py
+++ project/views.py
@@ -4,7 +4,20 @@ from project import app
 # Import functions from flask package
 from flask import render_template, redirect, url_for
 
+# Randomize choices
+from random import choice
+
 # Create new route at '/'
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+
+beginings = []
+middles1 = []
+middles2 = []
+ends = []
+
+@app.route('/story/<int:part>')
+def story(part):
     return render_template('home.html')
