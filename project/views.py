cec48bbf0a7c957f6070282da9a02f0dab666d51
calee14 <caplee1000@gmail.com>
2020-05-22 16:28:01 -0700

    middle two

--- project/views.py
+++ project/views.py
@@ -15,7 +15,7 @@ def home():
 
 beginings = []
 middles1 = ["The boys went out for a cold beer.", "Then, the family went to disneyland", "Everyone got hungry and ate their favorite candy bars, which was M&Ms.", ""]
-middles2 = []
+middles2 = ["This was the biggest mistake the people in the previous setence ever made.", "The underaged children snuck into the club from the back.", "The dwares started to grow wings."]
 ends = []
 
 @app.route('/story/<int:part>')
